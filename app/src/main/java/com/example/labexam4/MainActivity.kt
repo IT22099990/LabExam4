1a79e1e0513d5092c64cbac9d19021a0cc1f6db2
IT22099990 <it22099990@my.sliit.lk>
2024-05-11 02:17:27 +0530

    creating adapters and viewModel

--- app/src/main/java/com/example/labexam4/MainActivity.kt
+++ app/src/main/java/com/example/labexam4/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.labexam4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.labexam4.database.TaskDatabase
+import com.example.labexam4.repository.TaskRepository
+import com.example.labexam4.viewmodel.TaskViewModel
+import com.example.labexam4.viewmodel.TaskViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var taskViewModel: TaskViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupViewModel()
+
+    }
+
+    private fun setupViewModel(){
+        val taskRepository = TaskRepository(TaskDatabase(this))
+        val viewModelProviderFactory = TaskViewModelFactory(application, taskRepository)
+        taskViewModel = ViewModelProvider(this,viewModelProviderFactory)[TaskViewModel::class.java]
     }
 }
