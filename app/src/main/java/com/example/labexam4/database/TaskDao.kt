94b5b8b07912c447d9e46c4c162fd46e73ab1bf6
IT22099990 <it22099990@my.sliit.lk>
2024-05-11 00:31:08 +0530

    creating Room Database

---
+++ app/src/main/java/com/example/labexam4/database/TaskDao.kt
@@ -0,0 +1,30 @@
+package com.example.labexam4.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.labexam4.model.Task
+
+@Dao
+interface TaskDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)  //if the primary key already exist in the table old data will replace the new data
+    suspend fun insertTask(task: Task)   //insert tasks in the database
+
+    @Update
+    suspend fun updateTask(task: Task)  //update
+
+    @Delete
+    suspend fun deleteTask(task: Task)   //delete
+
+    @Query("SELECT * FROM TASKS ORDER BY id DESC")  //display all the tasks
+    fun getAllTasks(): LiveData<List<Task>>
+
+    @Query("SELECT * FROM TASKS WHERE taskTitle LIKE :query OR taskDesc LIKE :query")  //search a task
+    fun searchTask(query: String?): LiveData<List<Task>>
+
+
+}
